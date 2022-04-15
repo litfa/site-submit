bce3b4ebcc66a1e6959983933246eebaa7978905
Litfa <admin@litf.com.cn>
2022-04-15 19:58:59 +0800

    feat(Sites): 设置优秀站点

--- src/components/Sites/Sites.vue
+++ src/components/Sites/Sites.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: litfa
  * @Date: 2022-04-14 15:23:16
- * @LastEditTime: 2022-04-15 19:49:07
+ * @LastEditTime: 2022-04-15 19:57:58
  * @LastEditors: litfa
  * @Description: 网站
  * @FilePath: /web/src/components/Sites/Sites.vue
@@ -33,8 +33,19 @@ console.log(counter)
 
 <template>
   <div class="Sites">
+    <template v-if="goodList.length > 0">
+      <h1>优秀站点</h1>
+      <div class="box">
+        <Item
+          v-for="i in goodList"
+          :key="i.id"
+          v-bind="i"
+          :showAny="counter.permissions >= 15"
+        >{{ i.name }}</Item>
+      </div>
+    </template>
     <h1>所有站点</h1>
-    <div class="all">
+    <div class="box">
       <Item
         v-for="i in list"
         :key="i.id"
@@ -54,7 +65,7 @@ console.log(counter)
   h1 {
     margin: 20px;
   }
-  .all {
+  .box {
     display: grid;
     justify-content: space-between;
     grid-template-columns: repeat(auto-fill, 250px);
